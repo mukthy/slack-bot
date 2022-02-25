134db767ba8f9b410d57a7384ae3611738b63815
Muktheeswaran <muktheeswaran.m@gmail.com>
2022-02-25 14:39:03 +0530

    Updated the SlackBot and Added RegionCheck Feature

--- slackbot.py
+++ slackbot.py
@@ -20,7 +20,7 @@ client = WebClient(token=os.environ['SLACK_TOKEN'])
 api = os.environ['APIKEY']
 # common webhook where the data will be posted to the bot directly and it will be visible to everyone.
 slack_webhook_url = os.environ['SLACK_WEB_HOOK']
-
+spm_api = os.environ['SPM_API']
 headers = {
     'Content-type': 'application/json',
 }
@@ -182,6 +182,144 @@ def slack_help():
         url=slack_webhook_url, headers=headers, data=json.dumps(help_message))
     print(resp.status_code)
 
+    return Response(), 200
+
+# Region Checking Endpoint
+
+
+@app.route('/regioncheck', methods=['POST'])
+# the below function is to send a response as 200 to slack's post request within 3 sec to avoid the "operation_timed_out" error.
+def slack_crawlbot_response():
+    data = request.form
+    text = data.get('text')
+    validators.url(text)
+    user = data.get('user_name')
+    response_url = data["response_url"]
+    message = {
+        "text": "Connection successful!"
+    }
+    resp = requests.post(response_url, json=message)
+    print(resp.status_code)
+    crawl_bot = threading.Thread(
+        target=crawlbot,
+        args=(data, text, user, response_url)
+    )
+    crawl_bot.start()
+    return 'Processing, Please wait!!'
+
+
+def crawlbot(data, text, user, response_url):
+
+    print(data)
+    print(user)
+    if validators.url(text) is True:
+        regions = ['AU', 'AT', 'BY', 'BE', 'BO', 'BR', 'CA', 'CL', 'CN', 'DK', 'FI', 'FR', 'GE', 'DE', 'GR', 'HK', 'IN', 'ID', 'IE', 'IL', 'IT',
+                   'JP', 'MY', 'MX', 'NL', 'NG', 'PH', 'PL', 'PT', 'RO', 'RU', 'SG', 'ZA', 'KR', 'ES', 'SE', 'CH', 'TW', 'TH', 'TR', 'AE', 'GB', 'US', 'UY']
+
+        proxy_host = "proxy.crawlera.com"
+        proxy_port = "8010"
+        # Make sure to include ':' at the end
+        proxy_auth = f"{spm_api}:"
+        proxies = {
+            "https": "http://{}@{}:{}/".format(proxy_auth, proxy_host, proxy_port),
+            "http": "http://{}@{}:{}/".format(proxy_auth, proxy_host, proxy_port)
+        }
+        ok_result = []
+        error_result = []
+        threads = []
+        starting_region_check = {
+            "text": f"@{user} It may take upto 5 mins to complete, Go for a break or listen to a song :sweat_smile: "
+        }
+        requests.post(url=slack_webhook_url, json=starting_region_check)
+
+        def send(text, headers, region):
+
+            res = requests.get(url=f'{text}', headers=headers,
+                               proxies=proxies, verify='/Users/mukthy/Desktop/office/slack_bots/slack-bot_dev/zyte-proxy-ca.crt')
+            print(res.headers)
+            if 'x-crawlera-slave' in res.headers:
+                proxy_ip = res.headers['X-Crawlera-Slave']
+                print(
+                    f'The region {region} has a response of {res.status_code} status code from SPM. Proxy IP is {proxy_ip}')
+                ok_result.append(
+                    f'Country: {region} - Status: {res.status_code} - Proxy Details: {proxy_ip}')
+
+            elif 'X-Crawlera-Error' in res.headers:
+                proxy_ip = res.headers['X-Crawlera-Error']
+                print(
+                    f'The region {region} has a response of {res.status_code} status code from SPM. Proxy IP is {proxy_ip}')
+                error_result.append(
+                    f'Country: {region} - Status: {res.status_code} - Proxy Details: {proxy_ip}')
+
+        def main():
+
+            for region in regions:
+                headers = {
+                    'X-Crawlera-Region': f'{region}'
+                }
+                thread = threading.Thread(
+                    target=send,
+                    args=[text, headers, region]
+                )
+                thread.start()
+                threads.append(thread)
+
+            for thread in threads:
+                thread.join()
+
+            send(text, headers, region)
+
+        main()
+        print(ok_result)
+        print(error_result)
+        ok_result = json.dumps(ok_result)
+        error_result = json.dumps(error_result)
+        region_check_results = {
+            "text": 'Antibot Details',
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f'@{user} Region-check results for {text}:'
+                    }
+                },
+                {
+                    "type": "section",
+                    "block_id": "section567",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f"200 OK Results: \n {ok_result} \n\n Results with Errors: \n {error_result}"
+                    },
+                },
+            ]
+        }
+        resp = requests.post(
+            url=slack_webhook_url, headers=headers, data=json.dumps(region_check_results))
+        print(resp.status_code)
+
+        # message = {
+        #     "text": f"@{user} Proxy Region Check Result are below: \n\n {result} \n\n Do not Rely on this results alone. \n Adding specific headers, cookies and session may also help in avoiding 503."
+        # }
+        # resp = requests.post(url=slack_webhook_url, json=message)
+
+        print(resp.status_code)
+    else:
+        incorrect_url_warning = {
+            "text": 'Antibot Details',
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f'@{user} Please enter the proper URL like this http://{text} or https://{text} '
+                    }
+                },
+            ]
+        }
+        resp = requests.post(
+            url=response_url, headers=headers, data=json.dumps(incorrect_url_warning))
+        print(resp.status_code)
     return Response(), 200
 
 
