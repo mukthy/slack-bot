c788e9f528ec034b1b6ddd4d078b923dacb68632
Muktheeswaran <muktheeswaran.m@gmail.com>
2022-03-13 07:25:41 +0530

    Added Product and Article Auto-Extraction Function to SlackApp

--- slackbot.py
+++ slackbot.py
@@ -26,6 +26,8 @@ zyte_api_url = os.environ['ZYTE_DATA_API_URL']
 spm_api = os.environ['SPM_API']
 channel_id = os.environ['CHANNEL_ID']
 netloc_spm_api = os.environ['NETLOCK_SPM_API']
+auto_x_url = os.environ['AUTO_X_URL']
+auto_x_api = os.environ['AUTO_X_API']
 
 headers = {
     'Content-type': 'application/json',
@@ -526,6 +528,479 @@ def netlock_dc(data, text, user, response_url):
         netlock_dc_resp = requests.post(
             url=response_url, headers=headers, data=json.dumps(incorrect_url_warning))
         print(netlock_dc_resp.status_code)
+
+    return Response(), 200
+
+
+@app.route('/auto-x-product', methods=['POST'])
+# the below function is to send a response as 200 to slack's post request within 3 sec to avoid the "operation_timed_out" error.
+def slack_auto_x_product_response():
+    data = request.form
+    text = data.get('text')
+    validators.url(text)
+    user = data.get('user_name')
+    response_url = data["response_url"]
+    message = {
+        "text": "Connection successful!"
+    }
+    resp = requests.post(response_url, json=message)
+    print(resp.status_code)
+    auto_product = threading.Thread(
+        target=auto_x_product,
+        args=(data, text, user, response_url)
+    )
+    auto_product.start()
+    return 'Processing, Please wait!!'
+
+
+def auto_x_product(data, text, user, response_url):
+    if validators.url(text) is True:
+        print(data)
+        print(user)
+        url = f'{text}'
+
+        auto_x_product_start = {
+            "text": f'@{user}, Please wait Auto-Extraction is Running \n'
+        }
+        auto_x_product_response = requests.post(
+            url=response_url, headers=headers, data=json.dumps(auto_x_product_start))
+        print(auto_x_product_response.status_code)
+
+        response = requests.post(
+            f'{auto_x_url}',
+            auth=(f'{auto_x_api}', ''),
+            json=[{'url': f'{url}', 'pageType': 'product'}])
+        df = response.json()
+
+        # print(df)
+
+        if 'error' in df[0]:
+
+            print(df)
+            auto_x_product_list_results = {
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f'@{user} Auto-Extraction Product throwing error for {url} which is given below: \n\n {df}'
+                        }
+                    }
+                ]
+            }
+            auto_x_product_list_response = requests.post(
+                url=response_url, headers=headers, data=json.dumps(auto_x_product_list_results))
+            print(auto_x_product_list_response.status_code)
+
+        else:
+
+            # writing the response to the user.json file.
+            f = open(f'{user}.json', 'w')
+            f.write(str(json.dumps(df)))
+            f.close()
+
+            auto_x_product_msg = {
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f'@{user} Auto-Extraction Product results for {text} is given below: \n'
+                        }
+                    }
+                ]
+            }
+            auto_x_product_response = requests.post(
+                url=slack_webhook_url, headers=headers, data=json.dumps(auto_x_product_msg))
+            print(auto_x_product_response.status_code)
+
+            file_upload = client.files_upload(
+                channels=f'{channel_id}', filetype='json', file=f'{user}.json', title=f'{text}', user=f'{user}')
+            print(file_upload.status_code)
+
+            # gonna wait for 2 seconds after the upload and then delete the user.json to avoid space crunch on the server.
+            time.sleep(2)
+            os.remove(f'{user}.json')
+
+            # The below part of the code is to post the resutls directly to the Response URL or USER (Visible Only) istead of a json file.
+
+            # auto_x_product_results = {
+            #     "text": f'Results: \n\n {df}'
+            # }
+            # auto_x_product_results = requests.post(
+            #     url=response_url, headers=headers, data=json.dumps(auto_x_product_results))
+            # print(auto_x_product_results.status_code)
+    else:
+        incorrect_url_warning = {
+            "text": 'Antibot Details',
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f'@{user} Please enter the proper URL like this http://{text} or https://{text} '
+                    }
+                },
+            ]
+        }
+        auto_x_product_response = requests.post(
+            url=response_url, headers=headers, data=json.dumps(incorrect_url_warning))
+        print(auto_x_product_response.status_code)
+
+    return Response(), 200
+
+# Product Listing
+
+
+@app.route('/auto-x-product-listing', methods=['POST'])
+# the below function is to send a response as 200 to slack's post request within 3 sec to avoid the "operation_timed_out" error.
+def slack_auto_x_product_lisitng_response():
+    data = request.form
+    text = data.get('text')
+    validators.url(text)
+    user = data.get('user_name')
+    response_url = data["response_url"]
+    message = {
+        "text": "Connection successful!"
+    }
+    resp = requests.post(response_url, json=message)
+    print(resp.status_code)
+    auto_product_listing = threading.Thread(
+        target=auto_x_product_lisitng,
+        args=(data, text, user, response_url)
+    )
+    auto_product_listing.start()
+    return 'Processing, Please wait!!'
+
+
+def auto_x_product_lisitng(data, text, user, response_url):
+    if validators.url(text) is True:
+        print(data)
+        print(user)
+        url = f'{text}'
+
+        auto_x_product_list_start = {
+            "text": f'@{user}, Please wait Auto-Extraction is Running \n'
+        }
+        auto_x_product_list_response = requests.post(
+            url=response_url, headers=headers, data=json.dumps(auto_x_product_list_start))
+        print(auto_x_product_list_response.status_code)
+
+        response = requests.post(
+            f'{auto_x_url}',
+            auth=(f'{auto_x_api}', ''),
+            json=[{'url': f'{url}', 'pageType': 'productList'}])
+        df = response.json()
+
+        # print(df)
+        if 'error' in df[0]:
+
+            print(df)
+            auto_x_product_list_results = {
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f'@{user} Auto-Extraction Product List throwing error for {url} which is given below: \n\n {df}'
+                        }
+                    }
+                ]
+            }
+            auto_x_product_list_response = requests.post(
+                url=response_url, headers=headers, data=json.dumps(auto_x_product_list_results))
+            print(auto_x_product_list_response.status_code)
+
+        else:
+            # writing the response to the user.json file.
+            f = open(f'{user}.json', 'w')
+            f.write(str(json.dumps(df)))
+            f.close()
+
+            auto_x_product_list_msg = {
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f'@{user} Auto-Extraction Product Listing results for {text} is given below: \n'
+                        }
+                    }
+                ]
+            }
+            auto_x_product_list_response = requests.post(
+                url=slack_webhook_url, headers=headers, data=json.dumps(auto_x_product_list_msg))
+            print(auto_x_product_list_response.status_code)
+
+            file_upload = client.files_upload(
+                channels=f'{channel_id}', filetype='json', file=f'{user}.json', title=f'{text}', user=f'{user}')
+            print(file_upload.status_code)
+
+            # gonna wait for 2 seconds after the upload and then delete the user.json to avoid space crunch on the server.
+            time.sleep(2)
+            os.remove(f'{user}.json')
+
+            # The below part of the code is to post the resutls directly to the Response URL or USER (Visible Only) istead of a json file.
+
+            # auto_x_product_list_results = {
+            #     "text": f'Results: \n\n {df}'
+            # }
+            # auto_x_product_list_response = requests.post(
+            #     url=response_url, headers=headers, data=json.dumps(auto_x_product_list_results))
+            # print(auto_x_product_list_response.status_code)
+    else:
+        incorrect_url_warning = {
+            "text": 'Antibot Details',
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f'@{user} Please enter the proper URL like this http://{text} or https://{text} '
+                    }
+                },
+            ]
+        }
+        auto_x_product_list_response = requests.post(
+            url=response_url, headers=headers, data=json.dumps(incorrect_url_warning))
+        print(auto_x_product_list_response.status_code)
+
+    return Response(), 200
+
+# Article Only
+
+
+@app.route('/auto-x-article', methods=['POST'])
+# the below function is to send a response as 200 to slack's post request within 3 sec to avoid the "operation_timed_out" error.
+def slack_auto_x_article_response():
+    data = request.form
+    text = data.get('text')
+    validators.url(text)
+    user = data.get('user_name')
+    response_url = data["response_url"]
+    message = {
+        "text": "Connection successful!"
+    }
+    resp = requests.post(response_url, json=message)
+    print(resp.status_code)
+    auto_article = threading.Thread(
+        target=auto_x_article,
+        args=(data, text, user, response_url)
+    )
+    auto_article.start()
+    return 'Processing, Please wait!!'
+
+
+def auto_x_article(data, text, user, response_url):
+    if validators.url(text) is True:
+        print(data)
+        print(user)
+        url = f'{text}'
+
+        auto_x_article_starting = {
+            "text": f'@{user} Please wait Auto-Extraction is Running'
+        }
+        auto_x_article_response = requests.post(
+            url=response_url, headers=headers, data=json.dumps(auto_x_article_starting))
+        print(auto_x_article_response.status_code)
+
+        response = requests.post(
+            f'{auto_x_url}',
+            auth=(f'{auto_x_api}', ''),
+            json=[{'url': f'{url}', 'pageType': 'article'}])
+        df = response.json()
+
+        # print(df)
+
+        if 'error' in df[0]:
+
+            print(df)
+            auto_x_article_results = {
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f'@{user} Auto-Extraction Article throwing error for {url} which is given below: \n\n {df}'
+                        }
+                    }
+                ]
+            }
+            auto_x_article_response = requests.post(
+                url=response_url, headers=headers, data=json.dumps(auto_x_article_results))
+            print(auto_x_article_response.status_code)
+
+        else:
+
+            # writing the response to the user.json file.
+            f = open(f'{user}.json', 'w')
+            f.write(str(json.dumps(df)))
+            f.close()
+
+            auto_x_article_msg = {
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f'@{user} Auto-Extraction Article results for {text} is given below: \n'
+                        }
+                    }
+                ]
+            }
+            auto_x_article_response = requests.post(
+                url=slack_webhook_url, headers=headers, data=json.dumps(auto_x_article_msg))
+            print(auto_x_article_response.status_code)
+
+            file_upload = client.files_upload(
+                channels=f'{channel_id}', filetype='json', file=f'{user}.json', title=f'{text}', user=f'{user}')
+            print(file_upload.status_code)
+
+            # gonna wait for 2 seconds after the upload and then delete the user.json to avoid space crunch on the server.
+            time.sleep(2)
+            os.remove(f'{user}.json')
+
+            # The below part of the code is to post the resutls directly to the Response URL or USER (Visible Only) istead of a json file.
+
+            # auto_x_article_results = {
+            #     "text": f'Results: \n\n {df}'
+            # }
+            # auto_x_article_response = requests.post(
+            #     url=response_url, headers=headers, data=json.dumps(auto_x_article_results))
+            # print(auto_x_article_response.status_code)
+    else:
+        incorrect_url_warning = {
+            "text": 'Antibot Details',
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f'@{user} Please enter the proper URL like this http://{text} or https://{text} '
+                    }
+                },
+            ]
+        }
+        auto_x_article_response = requests.post(
+            url=response_url, headers=headers, data=json.dumps(incorrect_url_warning))
+        print(auto_x_article_response.status_code)
+
+    return Response(), 200
+
+
+@app.route('/auto-x-article-list', methods=['POST'])
+# the below function is to send a response as 200 to slack's post request within 3 sec to avoid the "operation_timed_out" error.
+def slack_auto_x_article_lisitng_response():
+    data = request.form
+    text = data.get('text')
+    validators.url(text)
+    user = data.get('user_name')
+    response_url = data["response_url"]
+    message = {
+        "text": "Connection successful!"
+    }
+    resp = requests.post(response_url, json=message)
+    print(resp.status_code)
+    auto_article_list = threading.Thread(
+        target=auto_x_article_lisitng,
+        args=(data, text, user, response_url)
+    )
+    auto_article_list.start()
+    return 'Processing, Please wait!!'
+
+
+def auto_x_article_lisitng(data, text, user, response_url):
+    if validators.url(text) is True:
+        print(data)
+        print(user)
+        url = f'{text}'
+
+        auto_x_article_list_starting = {
+            "text": f'@{user} Please wait Auto-Extraction is Running'
+        }
+        auto_x_article_list_response = requests.post(
+            url=response_url, headers=headers, data=json.dumps(auto_x_article_list_starting))
+        print(auto_x_article_list_response.status_code)
+
+        response = requests.post(
+            f'{auto_x_url}',
+            auth=(f'{auto_x_api}', ''),
+            json=[{'url': f'{url}', 'pageType': 'articleList'}])
+        df = response.json()
+
+        # print(df)
+
+        if 'error' in df[0]:
+
+            print(df)
+            auto_x_article_list_results = {
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f'@{user} Auto-Extraction Article List throwing error for {url} which is given below: \n\n {df}'
+                        }
+                    }
+                ]
+            }
+            auto_x_article_list_response = requests.post(
+                url=response_url, headers=headers, data=json.dumps(auto_x_article_list_results))
+            print(auto_x_article_list_response.status_code)
+
+        else:
+
+            f = open(f'{user}.json', 'w')
+            f.write(str(json.dumps(df)))
+            f.close()
+
+            auto_x_article_list_msg = {
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f'@{user} Auto-Extraction Article List results for {url} is given below: \n'
+                        }
+                    }
+                ]
+            }
+            auto_x_article_list_response = requests.post(
+                url=slack_webhook_url, headers=headers, data=json.dumps(auto_x_article_list_msg))
+            print(auto_x_article_list_response.status_code)
+
+            file_upload = client.files_upload(
+                channels=f'{channel_id}', filetype='json', file=f'{user}.json', title=f'{text}', user=f'{user}')
+            print(file_upload.status_code)
+
+            # gonna wait for 2 seconds after the upload and then delete the user.json to avoid space crunch on the server.
+            time.sleep(2)
+            os.remove(f'{user}.json')
+
+            # The below part of the code is to post the resutls directly to the Response URL or USER (Visible Only) istead of a json file.
+
+            # auto_x_article_list_results = {
+            #     "text": f'Results: \n\n {df}'
+            # }
+            # auto_x_article_list_response = requests.post(
+            #     url=response_url, headers=headers, data=json.dumps(auto_x_article_list_results))
+            # print(auto_x_article_list_response.status_code)
+    else:
+        incorrect_url_warning = {
+            "text": 'Antibot Details',
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f'@{user} Please enter the proper URL like this http://{text} or https://{text} '
+                    }
+                },
+            ]
+        }
+        auto_x_article_list_response = requests.post(
+            url=response_url, headers=headers, data=json.dumps(incorrect_url_warning))
+        print(auto_x_article_list_response.status_code)
 
     return Response(), 200
 
