a35e11f37fa8559667d6ac0cced3e0ceeaf257f4
Mukthy <muktheeswaran.m@gmail.com>
2022-05-13 06:42:19 +0530

    Fixed the 400 Bad Request error from Slack

--- modes/netloc_config.py
+++ modes/netloc_config.py
@@ -28,15 +28,15 @@ channel_id = os.environ['CHANNEL_ID']
 
 
 def default_netloc_config(url, user, slack_webhook_url, headers, response_url):
-
     query_url = f"{api_url}{url}"
-    payload = {}
     # print(query_url)
 
     payload = {}
 
+    header = {"Content-Type": "application/json"}
+
     response = requests.request(
-        "GET", url=url, auth=(django_user_api, ""), headers=headers, data=payload
+        "GET", url=query_url, auth=(django_user_api, ""), headers=headers, data=payload
     )
 
     #
@@ -68,89 +68,115 @@ def default_netloc_config(url, user, slack_webhook_url, headers, response_url):
         # print(res['count'])
         # page2 = res['next']
 
-        def datas(res, netloc):
-            data = res["results"][0:]
+        def datas(url):
+            data = netloc_api_result["results"][0:]
             # pprint.pprint(data[0])
             # print(len(data))
             # print(len(data[0]))
             # print(data[0]['organization_name'])
             num = int(len(data))
             for n in range(num):
-                # if 'Data on Demand' not in data[n]['organization_name']:
-                #     print(data[n]['organization_name'])
-                #     break
-                # elif 'Data on Demand' in data[n]['organization_name']:
-                #     pprint.pprint(data[n]['netloc_name'])
-                # print(n)
-                # pprint.pprint(data[n])
                 dict1 = data[n]
-                if "organization_name" not in dict1 and dict1["netloc_name"] == f"{netloc}":
+                if "organization_name" not in dict1 and f"{url}".lower() == dict1["netloc_name"]:
                     # print(dict1['organization_name'])
                     # pprint.pprint(dict1)
-                    dict2 = json.dumps(dict1, indent=4)
+
+                    # Spliting the BanRules and Other Configs of CCM separately because the JSON Post data to slack returns 400 Bad request.
+                    # dict2 = json.dumps(dict1, indent=4)
+                    banrules = dict1["ban_rules"]
+                    banrules_final = json.dumps(banrules, indent=4)
+                    # print(banrules_final)
+
+                    # The below part is for Other Configs (GL, SPW, Initial Jail Time etc) of CCM separately because the JSON Post data to slack returns 400 Bad request.
+                    rest_dict = {
+                        key: val for key, val in dict1.items() if key != "ban_rules"
+                    }
+                    rest_dict_final = json.dumps(rest_dict, indent=4)
+                    print(rest_dict_final)
+
                     # If we want to store the JSON in a file and post it in the #channel then un-comment from line 87 to 90 and uncomment from line 126 to 162
                     # f = open(f"{user}.json", "w")
                     # f.write(str(dict1))
                     # f.close()
                     # print("File Written")
-                else:
-                    print("Config does not exists")
 
+                    netloc_config_results = {
+                        "text": "Antibot Details",
+                        "blocks": [
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": f"@{user} Netloc-Config for {url} is below:\n {banrules_final}",
+                                },
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": f"{rest_dict_final}",
+                                },
+                            },
+                        ],
+                    }
+                    netloc_config_resps = requests.post(
+                        url=slack_webhook_url,
+                        headers=header,
+                        data=json.dumps(netloc_config_results),
+                    )
 
-        datas(netloc_api_result, url)
-        while netloc_api_result["next"] is not None:
-            next_page = netloc_api_result["next"]
-            # print(f"this is line 68 {next_page}")
-            response = requests.request(
-                "GET",
-                url=next_page,
-                auth=(django_user_api, ""),
-                headers=headers,
-                data=payload,
-            )
-            # print(response.text)
-            netloc_api_result = json.loads(response.text)
-            next_page = netloc_api_result["next"]
-            print(next_page)
-            datas(netloc_api_result, url)
-        # else res['next'] is None:
-        #     print('Next Page Does not Exist')
+                    # Leaving the below snippet as it was created due to the JSON post data to Slack was returning with 400 Bad Request.
+                    # rest_dict_final_results = {
+                    #     "text": "Antibot Details",
+                    #     "blocks": [
+                    #         {
+                    #             "type": "section",
+                    #             "text": {
+                    #                 "type": "mrkdwn",
+                    #                 "text": f"{rest_dict_final}",
+                    #             },
+                    #         },
+                    #     ],
+                    # }
+                    # rest_dict_final_resps = requests.post(
+                    #     url=response_url,
+                    #     headers=header,
+                    #     data=json.dumps(rest_dict_final_results),
+                    # )
+                    # print(rest_dict_final_resps)
 
-        # This below part of the code is used to upload a file with the Json result and delete it once it is uploaded to slack
-        # netloc_config_result = {
-        #     "text": "Antibot Details",
-        #     "blocks": [
-        #         {
-        #             "type": "section",
-        #             "text": {
-        #                 "type": "mrkdwn",
-        #                 "text": f"@{user} Netloc Config results for {url}:",
-        #             },
-        #         },
-        #         # {
-        #         #     "type": "section",
-        #         #     "block_id": "section567",
-        #         #     "text": {
-        #         #         "type": "mrkdwn",
-        #         #         "text": f"The Result is given below: \n\n {final_result}"
-        #         #     },
-        #         # },
-        #     ],
-        # }
-        # netloc_resp = requests.post(
-        #     url=slack_webhook_url,
-        #     headers=headers,
-        #     data=json.dumps(netloc_config_result),
-        # )
-        # file_upload = client.files_upload(
-        #     channels=f"{channel_id}",
-        #     filetype="json",
-        #     file=f"{user}.json",
-        #     title=f"{url}",
-        #     user=f"{user}",
-        # )
-        # print(file_upload.status_code)
-        # print(netloc_resp.status_code)
-        # time.sleep(2)
-        # os.remove(f"{user}.json")
-        # return netloc_resp
+                    # file_upload = client.files_upload(
+                    #     channels=f"{channel_id}",
+                    #     filetype="json",
+                    #     file=f"{user}.json",
+                    #     title=f"{url}",
+                    #     user=f"{user}",
+                    # )
+                    # print(file_upload.status_code)
+                    # print(netloc_resps.status_code)
+                    # time.sleep(2)
+                    # os.remove(f"{user}.json")
+                    print(netloc_config_resps)
+                    return netloc_config_resps
+            else:
+                print("Config does not exists")
+                netloc_config_result = {
+                    "text": "Antibot Details",
+                    "blocks": [
+                        {
+                            "type": "section",
+                            "text": {
+                                "type": "mrkdwn",
+                                "text": f"@{user} Netloc Config not present for {url}.\n Try with a Full Domain for ex: domain.com!",
+                            },
+                        },
+                    ],
+                }
+                netloc_resp = requests.post(
+                    url=response_url,
+                    headers=headers,
+                    data=json.dumps(netloc_config_result),
+                )
+                return netloc_resp
+
+        datas(url)
